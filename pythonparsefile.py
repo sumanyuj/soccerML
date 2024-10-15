9f68741c949db3b35024b40c4711b89e41054d46
richiezh-steiner <72473826+richiezh-steiner@users.noreply.github.com>
2024-10-15 18:07:13 -0400

    a start for now

--- pythonparsefile.py
+++ pythonparsefile.py
@@ -1,20 +1,14 @@
+import csv
 import pandas as pd
 import numpy as np
 #from sklearn.ensemble import RandomForestRegressor
 #from sklearn.datasets import make_regression
 #from sklearn.model_selection import train_test_split
 #from sklearn.metrics import mean_squared_error
-import pythonparsefile as mainfunction
 
-def predict_goals(csv):
-    x = 1
-
-
-def printHelloWorld():
-    print("Hello World")
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    mainfunction.parsing() #
-
-
-
+def parsing():
+    with open('cleaned_strikers_only.csv', mode ='r')as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            print(lines)
+            return lines 
