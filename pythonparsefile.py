cac489a5246476b105f48882356ed4debd1d5ba6
richiezh-steiner <72473826+richiezh-steiner@users.noreply.github.com>
2024-10-15 18:31:53 -0400

    updated db

--- pythonparsefile.py
+++ pythonparsefile.py
@@ -5,10 +5,10 @@ import numpy as np
 #from sklearn.datasets import make_regression
 #from sklearn.model_selection import train_test_split
 #from sklearn.metrics import mean_squared_error
-
+#use pandas to remove columns and remove data 
 def parsing():
     with open('cleaned_strikers_only.csv', mode ='r')as file:
         csvFile = csv.reader(file)
         for lines in csvFile:
             print(lines)
-            return lines 
+        return lines 
