6e3a4bcebc92b45d55ae1ea087fc90d20b6323cc
richiezh-steiner <72473826+richiezh-steiner@users.noreply.github.com>
2024-10-18 15:38:07 -0400

    psuedocode

--- pythonparsefile.py
+++ pythonparsefile.py
@@ -6,9 +6,13 @@ import numpy as np
 #from sklearn.model_selection import train_test_split
 #from sklearn.metrics import mean_squared_error
 #use pandas to remove columns and remove data 
+currentindex= 0
 def parsing():
     with open('cleaned_strikers_only.csv', mode ='r')as file:
-        csvFile = csv.reader(file)
-        for lines in csvFile:
+        csvFile = csv.reader(file) #reads the file
+        for lines in csvFile: 
+            lines.filter(items = [3], axis = 0) #get index 3, index 4, 5, 7,15,22
             print(lines)
         return lines 
+    with open('new.csv', 'w', newline='') as file:
+        newfile= csv.writer( ) #inputs the onlt filtered information 
