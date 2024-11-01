3fea82f4360bb7c55786a897581c536a8021e820
richiezh-steiner <72473826+richiezh-steiner@users.noreply.github.com>
2024-11-01 17:32:25 -0400

    got the necessary data for 2023-2024 is done

--- pythonparsefile.py
+++ pythonparsefile.py
@@ -11,7 +11,7 @@ data = pd.read_csv('clean2023-2024.csv' )
 #print(df)#goals_scored, assists, minutes, influence, threat, now_cost
 filtered_informaiton = data[[ 'first_name', 'second_name',  'goals_scored', 'assists', 'minutes', 'influence', 'threat', 'now_cost']]
 print(filtered_informaiton)
-filtered_informaiton.to_csv('clean.csv', index=False)
+filtered_informaiton.to_csv('clean_data2023-2024.csv', index=False)
 #used the various sources down below: https://www.geeksforgeeks.org/python-extracting-rows-using-pandas-iloc/, https://pandas.pydata.org/docs/user_guide/indexing.html, https://www.geeksforgeeks.org/filter-pandas-dataframe-based-on-index/
 #https://blog.hubspot.com/website/filter-rows-pandas, https://www.geeksforgeeks.org/python-extracting-rows-using-pandas-iloc/, https://www.geeksforgeeks.org/indexing-and-selecting-data-with-pandas/
 #additional resource that I used: https://www.datacamp.com/tutorial/save-as-csv-pandas-dataframe
